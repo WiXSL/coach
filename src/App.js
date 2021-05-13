4734a23c57df61cdda5c083d9794de4d411aa27f
asvarcas <asvarcas@etriek.com.uy>
2021-05-13 11:09:20 -0300

    Fix resources

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
-import { Admin, Resource } from 'react-admin';
+import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 
 import { PostList, PostEdit, PostCreate, PostShow } from './posts';
@@ -30,9 +30,8 @@ const App = () => {
             create={PostCreate}
             show={PostShow}
           />
-          <Resource name="users" icon={UserIcon} list={UserList}/>
-          <Resource name="comments" list={CommentList} show={CommentShow} edit={CommentEdit} create={CommentEdit}/>
-          <Resource name="albums" list={AlbumList} show={AlbumShow}/>
+          <Resource name="users" icon={UserIcon} list={UserList} />
+          <Resource name="comments" list={ListGuesser} />
       </Admin>
     );
 };
